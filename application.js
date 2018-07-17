caa3b42ece9c65d906c69ff1f6c1ad3a815de158
Stef Egbert <stephenegbert@gmail.com>
2018-07-17 19:31:36 -0400

    initial

---
+++ application.js
@@ -0,0 +1,88 @@
+function pkmn1() {
+  var audio = document.getElementById("audio");
+  audio.play();
+  var y = document.getElementById('pkmnPic');
+  y.classList.remove('animated');
+  y.classList.remove('lightSpeedIn');
+  void y.offsetWidth;
+  y.classList.add('animated');
+  y.classList.add('fadeInUp');
+  y.src = pkmnPics.one;
+  var x = document.getElementById('backHouse')
+  x.style.backgroundImage="url('images/fire.png')"
+}
+
+function pkmn2() {
+  var audio2 = document.getElementById("audio2");
+  audio2.play();
+  var y = document.getElementById('pkmnPic');
+    y.classList.remove('animated');
+  y.classList.remove('fadeInUp');
+  y.classList.add('animated');
+  y.classList.add('fadeInUp');
+  y.src = pkmnPics.two;
+  var x = document.getElementById('backHouse')
+  x.style.backgroundImage="url('images/water.png')"
+}
+
+function pkmn3() {
+  var audio3 = document.getElementById("audio3");
+  audio3.play();
+  var y = document.getElementById('pkmnPic');
+    y.classList.remove('animated');
+  y.classList.remove('fadeInUp');
+  y.classList.add('animated');
+  y.classList.add('fadeInUp');
+  y.src = pkmnPics.three;
+  var x = document.getElementById('backHouse');
+  x.style.backgroundImage="url('images/electric.png')";
+}
+
+function pkmn4() {
+  var audio4 = document.getElementById("audio4");
+  audio4.play();
+  var y = document.getElementById('pkmnPic');
+    y.classList.remove('animated');
+  y.classList.remove('fadeInUp');
+  y.classList.add('animated');
+  y.classList.add('fadeInUp');
+  y.src = pkmnPics.four;
+  var x = document.getElementById('backHouse')
+  x.style.backgroundImage="url('images/ground.png')"
+}
+
+function pkmn5() {
+  var audio5 = document.getElementById("audio5");
+  audio5.play();
+  var y = document.getElementById('pkmnPic');
+    y.classList.remove('animated');
+  y.classList.remove('fadeInUp');
+  y.classList.add('animated');
+  y.classList.add('fadeInUp');
+  y.src = pkmnPics.five;
+  var x = document.getElementById('backHouse')
+  x.style.backgroundImage="url('images/plant.png')"
+}
+
+function pkmn6() {
+  var audio6 = document.getElementById("audio6");
+  audio6.play();
+  var y = document.getElementById('pkmnPic');
+  y.classList.remove('animated');
+  y.classList.remove('lightSpeedIn');
+  void y.offsetWidth;
+  y.classList.add('animated');
+  y.classList.add('lightSpeedIn');
+  y.src = pkmnPics.six;
+  var x = document.getElementById('backHouse')
+  x.style.backgroundImage="url('images/ghost.png')"
+}
+
+pkmnPics = {
+  one: 'images/' + 'charizard.png',
+  two: 'images/' + 'Seadra.png',
+  three: 'images/' + 'jolteon.png',
+  four: 'images/' + 'sandslash.png',
+  five: 'images/' + 'exeggutor.png',
+  six: 'images/' + 'haunter.png',
+}
