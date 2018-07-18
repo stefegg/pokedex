8b060000b12bc4ccaedce985ea0f1e914c97f82b
Stef Egbert <stephenegbert@gmail.com>
2018-07-17 20:26:57 -0400

    2nd version with animations

--- application.js
+++ application.js
@@ -2,11 +2,16 @@ function pkmn1() {
   var audio = document.getElementById("audio");
   audio.play();
   var y = document.getElementById('pkmnPic');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
   y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
   y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
   void y.offsetWidth;
   y.classList.add('animated');
-  y.classList.add('fadeInUp');
+  y.classList.add('zoomInUp');
   y.src = pkmnPics.one;
   var x = document.getElementById('backHouse')
   x.style.backgroundImage="url('images/fire.png')"
@@ -16,10 +21,16 @@ function pkmn2() {
   var audio2 = document.getElementById("audio2");
   audio2.play();
   var y = document.getElementById('pkmnPic');
-    y.classList.remove('animated');
-  y.classList.remove('fadeInUp');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
+  y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
+  y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
+  void y.offsetWidth;
   y.classList.add('animated');
-  y.classList.add('fadeInUp');
+  y.classList.add('wobble');
   y.src = pkmnPics.two;
   var x = document.getElementById('backHouse')
   x.style.backgroundImage="url('images/water.png')"
@@ -29,10 +40,16 @@ function pkmn3() {
   var audio3 = document.getElementById("audio3");
   audio3.play();
   var y = document.getElementById('pkmnPic');
-    y.classList.remove('animated');
-  y.classList.remove('fadeInUp');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
+  y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
+  y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
+  void y.offsetWidth;
   y.classList.add('animated');
-  y.classList.add('fadeInUp');
+  y.classList.add('bounceInDown');
   y.src = pkmnPics.three;
   var x = document.getElementById('backHouse');
   x.style.backgroundImage="url('images/electric.png')";
@@ -42,10 +59,16 @@ function pkmn4() {
   var audio4 = document.getElementById("audio4");
   audio4.play();
   var y = document.getElementById('pkmnPic');
-    y.classList.remove('animated');
-  y.classList.remove('fadeInUp');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
+  y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
+  y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
+  void y.offsetWidth;
   y.classList.add('animated');
-  y.classList.add('fadeInUp');
+  y.classList.add('zoomInRight');
   y.src = pkmnPics.four;
   var x = document.getElementById('backHouse')
   x.style.backgroundImage="url('images/ground.png')"
@@ -55,10 +78,16 @@ function pkmn5() {
   var audio5 = document.getElementById("audio5");
   audio5.play();
   var y = document.getElementById('pkmnPic');
-    y.classList.remove('animated');
-  y.classList.remove('fadeInUp');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
+  y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
+  y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
+  void y.offsetWidth;
   y.classList.add('animated');
-  y.classList.add('fadeInUp');
+  y.classList.add('rubberBand');
   y.src = pkmnPics.five;
   var x = document.getElementById('backHouse')
   x.style.backgroundImage="url('images/plant.png')"
@@ -68,8 +97,13 @@ function pkmn6() {
   var audio6 = document.getElementById("audio6");
   audio6.play();
   var y = document.getElementById('pkmnPic');
+  y.classList.remove('wobble');
+  y.classList.remove('bounceInDown');
   y.classList.remove('animated');
+  y.classList.remove('zoomInUp');
   y.classList.remove('lightSpeedIn');
+  y.classList.remove('rubberBand');
+  y.classList.remove('zoomInRight');
   void y.offsetWidth;
   y.classList.add('animated');
   y.classList.add('lightSpeedIn');
